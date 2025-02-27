fe9add5d15c485da0bdf3bf9c565b9409db239e0
znoel93 <147119688+znoel93@users.noreply.github.com>
2025-02-26 21:30:46 -0500

    menu_user initial dropdown changes
    framework for feeding guide selection to dropdown menu

--- IntelChat/Pages/Menu_user.razor.cs
+++ IntelChat/Pages/Menu_user.razor.cs
@@ -1,4 +1,12 @@
+using IntelChat.Models;
+using IntelChat.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using ThreadingTask = System.Threading.Tasks.Task;
 
 namespace IntelChat.Pages
 {
@@ -15,5 +23,56 @@ namespace IntelChat.Pages
 		[Parameter]
 		[SupplyParameterFromQuery]
 		public int? pid { get; set; }
+
+		private List<Guide> entities = new List<Guide>();
+
+		protected override void OnInitialized()
+		{
+			LoadReadResults();
+		}
+
+		private SqlDataReader? ExecuteStoredProcedure(string procedure, List<SqlParameter> parameters, bool reader = false)
+		{
+			var connection = new SqlConnection(_configuration.GetValue<string>("ConnectionStrings:DefaultConnection"));
+			using var command = new SqlCommand(procedure, connection) { CommandType = CommandType.StoredProcedure };
+			parameters.ForEach(parameter => command.Parameters.Add(parameter));
+			connection.Open();
+			if (reader) return command.ExecuteReader(CommandBehavior.CloseConnection);
+			command.ExecuteNonQuery();
+			return null;
+		}
+
+		private SqlDataReader? Read(string status = "*")
+		{
+			List<SqlParameter> parameters = new List<SqlParameter>
+			{
+				new SqlParameter("@PROC_action", "Read"),
+				new SqlParameter("@PROC_filter", "****"),
+				new SqlParameter("@status", status),
+				new SqlParameter("@pod", pod)
+			};
+
+			return ExecuteStoredProcedure("dbo.[CRUD_Guide]", parameters, true);
+		}
+
+		/// <summary>Load entities from the database into a list </summary>
+		/// <param name="status">Status of the entities that will be loaded</param>
+		private void LoadReadResults(string status = "*")
+		{
+			var reader = Read(status);
+			if (reader == null) return;
+
+			entities.Clear();
+			while (reader.Read())
+			{
+				entities.Add(new Guide
+				{
+					GuideId = reader.GetInt32(0),
+					GuideLabel = reader.GetString(1)
+				});
+			}
+			reader.Close();
+		}
+
 	}
 }
