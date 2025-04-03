d3fbe6cdb6d698c873c54bcadea900d4c8dedd3d
znoel93 <147119688+znoel93@users.noreply.github.com>
2025-04-03 13:26:25 -0400

    page_invitation basic revamp
    starting point for revamp of page_invitation

--- IntelChat/Pages/Page_Invitation.razor.cs
+++ IntelChat/Pages/Page_Invitation.razor.cs
@@ -21,14 +21,14 @@ namespace IntelChat.Pages
 
 		[Parameter]
 		[SupplyParameterFromQuery]
-		public int? registrationId { get; set; } // Noun ID for navigation
+		public int? brandId { get; set; } // Noun ID for navigation
 
 		[Inject]
 		public NotificationService NotificationService { get; set; }
 		private string? show { get; set; }
 		private List<Pype> pypes = new List<Pype>();
-		private List<Registration> entities = new List<Registration>();
-		private Dictionary<String, Registration> entity = new Dictionary<String, Registration>();
+		private List<Brand> entities = new List<Brand>();
+		private Dictionary<String, Brand> entity = new Dictionary<String, Brand>();
 		private Dictionary<String, String> filter = new Dictionary<String, String>();
 
 		/// <summary>Executes a stored procedure and (optionally) returns a reader for its results</summary>
@@ -56,15 +56,26 @@ namespace IntelChat.Pages
 			List<SqlParameter> parameters = new List<SqlParameter>
 			{
 				new SqlParameter("@PROC_action", "Create"),
-				//new SqlParameter("@pod", pod),
-				new SqlParameter("@id", entity["add"].RegistrationId),
-				new SqlParameter("@username", entity["add"].RegistrationUsername),
-				new SqlParameter("@password", entity["add"].RegistrationStatus),
-				new SqlParameter("@email", entity["add"].RegistrationEmail),
-				new SqlParameter("@registration_status", entity["add"].RegistrationStatus),
-				new SqlParameter("@person_id_fk", entity["add"].PersonIdFk)
+				new SqlParameter("@pod", pod),
+				new SqlParameter("@brand_code", entity["add"].BrandCode),
+				new SqlParameter("@brand_label", entity["add"].BrandLabel),
+				new SqlParameter("@brand_image", entity["add"].BrandImage),
+				new SqlParameter("@brand_status", entity["add"].BrandStatus),
+				new SqlParameter("@brand_cnt_max", entity["add"].BrandCntMax),
+				new SqlParameter("@brand_cnt_reg", 0),
+				new SqlParameter("@brand_eligibility", entity["add"].BrandEligibility),
+				new SqlParameter("@brand_reg_date_closed", entity["add"].BrandRegDateClosed),
+				new SqlParameter("@brand_cost", entity["add"].BrandCost),
+				new SqlParameter("@brand_guide", entity["add"].BrandGuide),
+				new SqlParameter("@brand_role", entity["add"].BrandRole),
+				new SqlParameter("@nova_id_fk", entity["add"].NovaIdFk),
+				new SqlParameter("@program_id_fk", pod),
+				new SqlParameter("@location_id_fk", entity["add"].LocationIdFk),
+				new SqlParameter("@channel_alpha", entity["add"].ChannelAlpha),
+				new SqlParameter("@channel_beta", entity["add"].ChannelBeta),
+				new SqlParameter("@channel_gamma", entity["add"].ChannelGamma)
 			};
-			ExecuteStoredProcedure("dbo.[CRUD_Registration]", parameters);
+			ExecuteStoredProcedure("dbo.[CRUD_Brand]", parameters);
 		}
 
 		/// <summary>Reads entities from the database using a stored procedure</summary>
@@ -74,34 +85,43 @@ namespace IntelChat.Pages
 		{
 			List<SqlParameter> parameters = new List<SqlParameter>
 			{
-				new SqlParameter("@PROC_action", "Read_All"),
-				//new SqlParameter("@PROC_filter", "****"),
-				new SqlParameter("@registration_status", status),
-
+				new SqlParameter("@PROC_action", "Read"),
+				new SqlParameter("@PROC_filter", "****"),
+				new SqlParameter("@brand_status", status),
+				new SqlParameter("@pod", pod)
 			};
-			return ExecuteStoredProcedure("dbo.[CRUD_Registration]", parameters, true);
+			return ExecuteStoredProcedure("dbo.[CRUD_Brand]", parameters, true);
 		}
 
 		/// <summary>Load entities from the database into a list </summary>
 		/// <param name="status">Status of the entities that will be loaded</param>
-		private void LoadReadResults(string status = "*")
+		private void LoadReadResults(string status = "*", string filter = "****")
 		{
-			var reader = Read(status);
+			var reader = Read(status, filter);
 			if (reader == null) return;
 
 			entities.Clear();
 			while (reader.Read())
 			{
-				entities.Add(new Registration
+				entities.Add(new Brand
 				{
-					RegistrationId = reader.GetInt32(0),
-					RegistrationUsername = reader.GetString(1),
-					RegistrationPassword = reader.GetString(2),
-					RegistrationEmail = reader.GetString(3),
-					RegistrationStatus = reader.GetString(4),
-					PersonIdFk = reader.GetInt32(5)
+					BrandId = reader.GetInt32(0),
+					BrandCode = reader.GetString(1),
+					BrandLabel = reader.GetString(2),
+					BrandImage = reader.GetString(20),
+					BrandStatus = reader.GetString(3),
+					BrandCntMax = reader.GetInt16(4),
+					BrandCntReg = reader.GetInt16(5),
+					BrandEligibility = reader.GetInt16(8),
+					BrandRegDateClosed = reader.GetDateTime(6),
+					BrandCost = reader.GetDecimal(9),
+					GuideIdFk = reader.GetInt32(13),
+					BrandRole = reader.GetString(7),
+					NovaIdFk = reader.GetInt32(14),
+					ProgramIdFk = reader.GetInt32(12),
+					LocationIdFk = reader.GetInt32(11),
+					PodIdFk = reader.GetInt32(16)
 				});
-				
 			}
 			reader.Close();
 		}
@@ -113,8 +133,6 @@ namespace IntelChat.Pages
 			filter[type] = args.Value.ToString();
 			var entitiesFiltered = entities;
 			if (!entitiesFiltered.Any()) return;
-			if (status != "*") entitiesFiltered = (List<Registration>)entitiesFiltered.Where(entity => entity.RegistrationStatus == status);
-			if (!entitiesFiltered.Any()) return;
 			entity[type] = (entitiesFiltered == null) ? entities.First() : entitiesFiltered.First();
 		}
 
@@ -124,15 +142,27 @@ namespace IntelChat.Pages
 			List<SqlParameter> parameters = new List<SqlParameter>
 			{
 				new SqlParameter("@PROC_action", "Update"),
-				//new SqlParameter("@pod", pod),
-				new SqlParameter("@id", entity["change"].RegistrationId),
-				new SqlParameter("@username", entity["change"].RegistrationUsername),
-				new SqlParameter("@password", entity["change"].RegistrationStatus),
-				new SqlParameter("@email", entity["change"].RegistrationEmail),
-				new SqlParameter("@registration_status", entity["change"].RegistrationStatus),
-				new SqlParameter("@person_id_fk", entity["change"].PersonIdFk)
+				new SqlParameter("@id", entity["change"].BrandId),
+				new SqlParameter("@pod", pod),
+				new SqlParameter("@brand_code", entity["change"].BrandCode),
+				new SqlParameter("@brand_label", entity["change"].BrandLabel),
+				new SqlParameter("@brand_image", entity["change"].BrandImage),
+				new SqlParameter("@brand_status", entity["change"].BrandStatus),
+				new SqlParameter("@brand_cnt_max", entity["change"].BrandCntMax),
+				new SqlParameter("@brand_cnt_reg", entity["change"].BrandCntReg),
+				new SqlParameter("@brand_eligibility", entity["change"].BrandEligibility),
+				new SqlParameter("@brand_reg_date_closed", entity["change"].BrandRegDateClosed),
+				new SqlParameter("@brand_cost", entity["change"].BrandCost),
+				new SqlParameter("@brand_guide", entity["change"].BrandGuide),
+				new SqlParameter("@brand_role", entity["change"].BrandRole),
+				new SqlParameter("@nova_id_fk", entity["change"].NovaIdFk),
+				new SqlParameter("@program_id_fk", pod),
+				new SqlParameter("@location_id_fk", entity["change"].LocationIdFk),
+				new SqlParameter("@channel_alpha", entity["change"].ChannelAlpha),
+				new SqlParameter("@channel_beta", entity["change"].ChannelBeta),
+				new SqlParameter("@channel_gamma", entity["change"].ChannelGamma)
 			};
-			ExecuteStoredProcedure("dbo.[CRUD_Registration]", parameters);
+			ExecuteStoredProcedure("dbo.[CRUD_Brand]", parameters);
 		}
 
 		/// <summary>Delete an entity in the database using a stored procedure</summary>
@@ -141,52 +171,50 @@ namespace IntelChat.Pages
 			List<SqlParameter> parameters = new List<SqlParameter>
 			{
 				new SqlParameter("@PROC_action", "Delete"),
-				new SqlParameter("@id", entity["delete"].RegistrationId),
-				new SqlParameter("@registration_status", entity["delete"].RegistrationStatus)
+				new SqlParameter("@id", entity["delete"].BrandId),
+				new SqlParameter("@brand_status", entity["delete"].BrandStatus)
 			};
-			ExecuteStoredProcedure("dbo.[CRUD_Registration]", parameters);
+			ExecuteStoredProcedure("dbo.[CRUD_Brand]", parameters);
 		}
 
 		/// <summary>Handle events triggered by entity creation</summary>
 		private void OnCreate()
 		{
 			Create();
-			entities.Add(entity["add"]);
-			NotificationService.Notify("Registration created successfully!", NotificationType.Success);
+			LoadReadResults();
+			NotificationService.Notify("Brand created successfully!", NotificationType.Success);
 		}
 
 		/// <summary>Handle events triggered by entity changes</summary>
 		private void OnChange()
 		{
 			Change();
-			entities.Remove(entities.Find(e => e.RegistrationId == entity["change"].RegistrationId));
-			entities.Add(entity["change"]);
-			NotificationService.Notify("Registration changed successfully!", NotificationType.Success);
+			LoadReadResults();
+			NotificationService.Notify("Brand changed successfully!", NotificationType.Success);
 		}
 
 		/// <summary>Handle events triggered by entity deletions</summary>
 		private void OnDelete()
 		{
 			Delete();
-			entities.Remove(entities.Find(e => e.RegistrationId == entity["delete"].RegistrationId));
-			NotificationService.Notify("Registration deleted successfully!", NotificationType.Success);
-			show = "list";
+			LoadReadResults();
+			NotificationService.Notify("Brand deleted successfully!", NotificationType.Success);
 			show = "list";
 		}
 
 		/// <summary>Fill fields in the change tab based on entity selection</summary>
 		private void AutoFill(int id, String type)
 		{
-			var target = entities.Find(e => e.RegistrationId == id);
+			var target = entities.Find(e => e.BrandId == id);
 			if (target != null) entity[type] = target;
 		}
 
-		/// <summary>use the Pype table Pype_ID to list valid Pype Types via a stored procedure</summary>
+		/// <summary>Reads pypes from the database using a stored procedure</summary>
 		/// <returns>Reader for the entities that were read from the database</returns>
 		private SqlDataReader? ReadPype()
 		{
 			List<SqlParameter> parameters = new List<SqlParameter> {
-				new SqlParameter("@PROC_Input_Filter", "role"),
+				new SqlParameter("@PROC_Input_Filter", "BRND"),
 				new SqlParameter("@pod", pod)
 			};
 			return ExecuteStoredProcedure("dbo.[sp_Pype_Type_Locked]", parameters, true);
@@ -216,18 +244,20 @@ namespace IntelChat.Pages
 
 		public void NavigateToLascaux(bool isSubject)
 		{
-			_nav.NavigateTo(String.Format("/Lascaux?pod={0}&pid={1}&prevPage={2}&type={3}", pod, pid, "Registration", "Registration"), true);
+			_nav.NavigateTo(String.Format("/Lascaux?pod={0}&pid={1}&prevPage={2}&type={3}", pod, pid, "Brand", "Brand"), true);
 		}
 
 		protected override void OnInitialized()
 		{
-			entity["add"] = new Registration();
-			entity["change"] = new Registration();
-			entity["delete"] = new Registration();
+			// Initialize default entities and filters
+			entity["add"] = new Brand();
+			entity["change"] = new Brand();
+			entity["delete"] = new Brand();
 			filter["list"] = "****";
 			filter["change"] = "****";
 			filter["delete"] = "****";
 
+			// Load data for Brand entities
 			LoadReadResults();
 			LoadReadPypeResults();
 
@@ -237,23 +267,23 @@ namespace IntelChat.Pages
 				switch (show)
 				{
 					case "change":
-						if (registrationId.HasValue)
+						if (brandId.HasValue)
 						{
-							AutoFill(registrationId.Value, "change"); // Populate fields for specific NounId
+							AutoFill(brandId.Value, "change"); // Populate fields for specific BrandId
 						}
 						else if (entities.Any())
 						{
 							entity["change"] = entities.First();
-							AutoFill(entity["change"].RegistrationId, "change"); // Default to first entity
+							AutoFill(entity["change"].BrandId, "change"); // Default to first entity
 						}
 						break;
 
 					case "delete":
-						var deletedEntity = entities.FirstOrDefault(e => e.RegistrationStatus == "D");
+						var deletedEntity = entities.FirstOrDefault(e => e.BrandStatus == "D");
 						if (deletedEntity != null)
 						{
 							entity["delete"] = deletedEntity;
-							AutoFill(deletedEntity.RegistrationId, "delete"); // Populate fields for the deleted entity
+							AutoFill(deletedEntity.BrandId, "delete"); // Populate fields for the deleted entity
 						}
 						break;
 
@@ -272,11 +302,12 @@ namespace IntelChat.Pages
 				if (entities.Any())
 				{
 					entity["change"] = entities.First();
-					AutoFill(entity["change"].RegistrationId, "change");
+					AutoFill(entity["change"].BrandId, "change");
 				}
 				show = string.Empty; // Do not show the list by default
 			}
 		}
+
 
 		private void OnItemSelected(int id)
 		{
@@ -298,7 +329,7 @@ namespace IntelChat.Pages
 			if (e.Key == "Enter" && int.TryParse(directSelectId, out int id))
 			{
 				// Find the entity by ID and navigate to the change screen
-				var selectedEntity = entities.FirstOrDefault(entity => entity.RegistrationId == id);
+				var selectedEntity = entities.FirstOrDefault(entity => entity.BrandId == id);
 				if (selectedEntity != null)
 				{
 					AutoFill(id, "change"); // Populate fields with selected entity
@@ -311,5 +342,26 @@ namespace IntelChat.Pages
 				}
 			}
 		}
-    }
+
+
+		private string tagFilter { get; set; } = string.Empty;
+		private SqlDataReader? Read(string status = "*", string filter = "****")
+		{
+			List<SqlParameter> parameters = new List<SqlParameter>
+			{
+				new SqlParameter("@PROC_action", "Read"),
+				new SqlParameter("@PROC_filter", filter),
+				new SqlParameter("@brand_status", status),
+				new SqlParameter("@pod", pod)
+			};
+
+			return ExecuteStoredProcedure("dbo.[CRUD_Brand]", parameters, true);
+		}
+
+		private void ApplyTagFilter(ChangeEventArgs e)
+		{
+			tagFilter = e.Value?.ToString() ?? string.Empty;
+			LoadReadResults("*", tagFilter);
+		}
+	}
 }
